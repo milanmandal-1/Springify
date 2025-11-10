f673396eb4dda3bc93af536dd8e3ef645dfcaed1
milanmandal-1 <engr.mm19@gmail.com>
2025-11-11 00:16:22 +0530

    springMVC

---
+++ src/main/java/com/springProject/springboot/cruddemo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.springProject.springboot.cruddemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+        theModel.addAttribute("theDate", LocalDateTime.now());
+        return "helloworld";  // looks for helloworld.html in templates
+    }
+}
--- src/main/java/com/springProject/springboot/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springProject.springboot.cruddemo.dao;
-
-import com.springProject.springboot.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-    //hdacv
-}
